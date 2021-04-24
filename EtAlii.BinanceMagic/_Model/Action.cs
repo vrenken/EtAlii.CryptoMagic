37b1685af840f88a4988f2978b19eafa15b5a18d
Peter Vrenken <peter.vrenken@atos.net>
2021-04-24 12:36:05 +0200

    Consolidated action records.

--- EtAlii.BinanceMagic/_Model/Action.cs
+++ EtAlii.BinanceMagic/_Model/Action.cs
@@ -1,6 +1,9 @@
-﻿namespace EtAlii.BinanceMagic
+namespace EtAlii.BinanceMagic
 {
-    public record BuyAction
+    public record BuyAction : Action;
+    public record SellAction : Action;
+
+    public abstract record Action
     {
         public string Coin { get; init; }
         public decimal Quantity { get; init; }
