00a1e3aa7e6059827f7caf0a4cf1343d58b838f2
Peter Vrenken <peter.vrenken@atos.net>
2021-05-13 23:02:43 +0200

    Added service project.

---
+++ EtAlii.BinanceMagic.Service/Shared/NavMenu.cs
@@ -0,0 +1,14 @@
+namespace EtAlii.BinanceMagic.Service.Shared
+{
+    public partial class NavMenu
+    {
+        private bool _collapseNavMenu = true;
+
+        private string NavMenuCssClass => _collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu()
+        {
+            _collapseNavMenu = !_collapseNavMenu;
+        }
+    }
+}
