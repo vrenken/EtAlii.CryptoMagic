a69846f36eb47ca4679353873f649796315ae9fd
Peter Vrenken <peter.vrenken@atos.net>
2021-05-13 23:21:30 +0200

    Simplified project.

---
+++ EtAlii.BinanceMagic.Service/Shared/SurveyPrompt.cs
@@ -0,0 +1,11 @@
+﻿namespace EtAlii.BinanceMagic.Service.Shared
+{
+    using Microsoft.AspNetCore.Components;
+
+    public partial class SurveyPrompt
+    {
+        // Demonstrates how a parent component can supply parameters
+        [Parameter]
+        public string Title { get; set; }
+    }
+}
