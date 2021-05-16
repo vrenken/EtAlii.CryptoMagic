2bb8a36ed8a704ce0c00dc6fd8e4cda29fbec79c
Peter Vrenken <peter.vrenken@atos.net>
2021-05-16 14:24:10 +0200

    Steps forward

---
+++ EtAlii.BinanceMagic.Service/Pages/CircularEdit.cs
@@ -0,0 +1,57 @@
+﻿namespace EtAlii.BinanceMagic.Service.Pages
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using EtAlii.BinanceMagic.Service.Data;
+
+    public partial class CircularEdit
+    {
+        private readonly Experiment[] _experiments;
+        private Experiment _currentExperiment;
+
+        public CircularEdit()
+        {
+            _experiments = new[]
+            {
+                new Experiment { Name = "Circular: BTC-BNB", Steps = new []{ new ExperimentStep(), new ExperimentStep(), } },
+                new Experiment { Name = "Circular: LTC-XMR" },
+                new Experiment { Name = "Surfing: BTC-BNB-XMR" },
+            };
+            _currentExperiment = _experiments.First();
+            UpdateExperiments();
+        }
+        
+        private void SelectExperiment(string experimentName)
+        {
+            InvokeAsync(() =>
+            {
+                _currentExperiment = _experiments.SingleOrDefault(e => e.Name == experimentName);
+                
+                UpdateExperiments();
+                StateHasChanged();
+            });
+        }
+
+        private void UpdateExperiments()
+        {
+            foreach (var experiment in _experiments)
+            {
+                experiment.IsActive = _currentExperiment == experiment;
+            }
+        }
+        
+        protected override Task OnInitializedAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            _algorithmRunnerService.Tick += () =>
+            {
+                InvokeAsync(StateHasChanged);
+            };
+        }
+    }
+}
