d245e681af1ffe9003ab3999094a4e54a76ab1e7
Peter Vrenken <peter.vrenken@atos.net>
2021-05-21 17:19:28 +0200

    Added simple OneOffView

---
+++ EtAlii.BinanceMagic.Service/Trading/OneOff/OneOffView.cs
@@ -0,0 +1,14 @@
+﻿namespace EtAlii.BinanceMagic.Service
+{
+    using System;
+    using System.Linq;
+
+    public partial class OneOffView
+    {
+        protected override OneOffTrading GetTrading(Guid id)
+        {
+            using var data = new DataContext();
+            return data.OneOffTradings.Single(t => t.Id == id);
+        }
+    }
+}
