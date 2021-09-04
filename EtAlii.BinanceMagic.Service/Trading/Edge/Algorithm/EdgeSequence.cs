9690edb79bc9682144c80fe79c1c59bc25e56b93
Peter Vrenken <peter.vrenken@atos.net>
2021-09-04 20:38:25 +0200

    Steps forward.

--- EtAlii.BinanceMagic.Service/Trading/Edge/Algorithm/EdgeSequence.cs
+++ EtAlii.BinanceMagic.Service/Trading/Edge/Algorithm/EdgeSequence.cs
@@ -5,15 +5,15 @@ namespace EtAlii.BinanceMagic.Service
     public partial class EdgeSequence 
     {
         // private readonly IClient _client;
-        private readonly ITimeManager _timeManager;
-        private readonly IAlgorithmContext<EdgeTransaction, EdgeTrading> _context;
-        private readonly ITargetTransactionFinder _targetTransactionFinder;
+        // private readonly ITimeManager _timeManager;
+        // private readonly IAlgorithmContext<EdgeTransaction, EdgeTrading> _context;
+        // private readonly ITargetTransactionFinder _targetTransactionFinder;
 
-        public EdgeSequence(ITimeManager timeManager, IAlgorithmContext<EdgeTransaction, EdgeTrading> context, ITargetTransactionFinder targetTransactionFinder)
-        {
-            _timeManager = timeManager;
-            _context = context;
-            _targetTransactionFinder = targetTransactionFinder;
-        }
+        // public EdgeSequence(ITimeManager timeManager, IAlgorithmContext<EdgeTransaction, EdgeTrading> context, ITargetTransactionFinder targetTransactionFinder)
+        // {
+        //     _timeManager = timeManager;
+        //     _context = context;
+        //     _targetTransactionFinder = targetTransactionFinder;
+        // }
     }
 }
